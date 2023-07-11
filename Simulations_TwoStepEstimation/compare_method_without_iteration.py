de9606b30710adeb96009ebeafe6548e9cf4769b
Perrine Chassat <perrine.chassat@gmail.com>
2023-07-11 18:32:29 +0200

    simu

--- Simulations_TwoStepEstimation/compare_method_without_iteration.py
+++ Simulations_TwoStepEstimation/compare_method_without_iteration.py
@@ -29,33 +29,39 @@ def compare_method_without_iteration(theta, arc_length_fct, N, Gamma, mu0, P0, n
     X = Z[:,:3,3]
     Y = X + np.random.multivariate_normal(np.zeros(3), Gamma, size=N)
 
-    ## Init Gamma and s(t)
-    derivatives, h_opt = compute_derivatives(Y, grid_time, deg=3, h=None, CV_optimization_h={"flag":True, "h_grid":np.array([bounds_h[0], bounds_h[-1]]), "K":10, "method":'bayesian', "n_call":30, "verbose":False})
-    grid_arc_s, L, arc_s, arc_s_dot = compute_arc_length(Y, grid_time, smooth=True, smoothing_param=h_opt)
+    try:
 
-    ## Z GramSchmidt
-    GS_orthog = GramSchmidtOrthogonalization(Y, grid_arc_s, deg=3)
-    h_opt = GS_orthog.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
-    Z_hat_GS, Q_hat_GS, X_hat_GS = GS_orthog.fit(h_opt) 
+        ## Init Gamma and s(t)
+        derivatives, h_opt = compute_derivatives(Y, grid_time, deg=3, h=None, CV_optimization_h={"flag":True, "h_grid":np.array([bounds_h[0], bounds_h[-1]]), "K":10, "method":'bayesian', "n_call":30, "verbose":False})
+        grid_arc_s, L, arc_s, arc_s_dot = compute_arc_length(Y, grid_time, smooth=True, smoothing_param=h_opt)
 
-    ## Z CLP
-    CLP_reg = ConstrainedLocalPolynomialRegression(Y, grid_arc_s, adaptative=False, deg_polynomial=3)
-    h_opt = CLP_reg.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
-    Z_hat_CLP, Q_hat_CLP, X_hat_CLP = CLP_reg.fit(h_opt)
-   
-    ## theta extrinsic
-    extrins_model_theta = ExtrinsicFormulas(Y, grid_time, grid_arc_s, deg_polynomial=3)
-    h_opt, lbda_opt = extrins_model_theta.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
-    Basis_theta_hat_extrins = extrins_model_theta.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
+        ## Z GramSchmidt
+        GS_orthog = GramSchmidtOrthogonalization(Y, grid_arc_s, deg=3)
+        h_opt = GS_orthog.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
+        Z_hat_GS, Q_hat_GS, X_hat_GS = GS_orthog.fit(h_opt) 
 
-    ## theta GS + LS
-    local_approx_ode = LocalApproxFrenetODE(grid_arc_s, Z=Z_hat_GS)
-    h_opt, lbda_opt = local_approx_ode.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
-    Basis_theta_hat_GS_LS = local_approx_ode.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
+        ## Z CLP
+        CLP_reg = ConstrainedLocalPolynomialRegression(Y, grid_arc_s, adaptative=False, deg_polynomial=3)
+        h_opt = CLP_reg.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
+        Z_hat_CLP, Q_hat_CLP, X_hat_CLP = CLP_reg.fit(h_opt)
+    
+        ## theta extrinsic
+        extrins_model_theta = ExtrinsicFormulas(Y, grid_time, grid_arc_s, deg_polynomial=3)
+        h_opt, lbda_opt = extrins_model_theta.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
+        Basis_theta_hat_extrins = extrins_model_theta.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
 
-    ## theta CLP + LS
-    local_approx_ode = LocalApproxFrenetODE(grid_arc_s, Z=Z_hat_CLP)
-    h_opt, lbda_opt = local_approx_ode.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
-    Basis_theta_hat_CLP_LS = local_approx_ode.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
+        ## theta GS + LS
+        local_approx_ode = LocalApproxFrenetODE(grid_arc_s, Z=Z_hat_GS)
+        h_opt, lbda_opt = local_approx_ode.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
+        Basis_theta_hat_GS_LS = local_approx_ode.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
 
-    return Y, Z, Z_hat_GS, Z_hat_CLP, Basis_theta_hat_extrins, Basis_theta_hat_GS_LS, Basis_theta_hat_CLP_LS
+        ## theta CLP + LS
+        local_approx_ode = LocalApproxFrenetODE(grid_arc_s, Z=Z_hat_CLP)
+        h_opt, lbda_opt = local_approx_ode.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False)
+        Basis_theta_hat_CLP_LS = local_approx_ode.Bspline_smooth_estimates(h_opt, nb_basis, regularization_parameter=lbda_opt)
+    
+        return Y, Z, Z_hat_GS, Z_hat_CLP, Basis_theta_hat_extrins, Basis_theta_hat_GS_LS, Basis_theta_hat_CLP_LS
+    
+    except:
+
+        return None
