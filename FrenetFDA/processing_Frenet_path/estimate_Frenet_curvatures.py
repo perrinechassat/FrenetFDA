ee773eee4727c2bc6050c84619b80f1ff1f5e6cf
Perrine Chassat <perrine.chassat@gmail.com>
2023-07-18 16:45:11 +0200

    simu

--- FrenetFDA/processing_Frenet_path/estimate_Frenet_curvatures.py
+++ FrenetFDA/processing_Frenet_path/estimate_Frenet_curvatures.py
@@ -585,6 +585,7 @@ class TwoStepEstimatorTracking:
         Dtheta = theta
         k=0
         while np.linalg.norm(Dtheta)>=epsilon*np.linalg.norm(theta) and k<max_iter:
+            print('iteration smoother n°', k)
             theta_old = theta
             Smoother = TrackingSmootherLinear(grid, Q=Q_smooth)
             Q_smooth = Smoother.fit(lbda_track, basis_theta.evaluate)
@@ -599,8 +600,6 @@ class TwoStepEstimatorTracking:
 
     def bayesian_optimization_hyperparameters(self, n_call_bayopt, lambda_track_bounds, lambda_bounds, h_bounds, nb_basis, order=4, epsilon=1e-03, max_iter=30, n_splits=10, verbose=True):
 
-        # ## CV optimization of lambda
-
         def func(x):
             score = np.zeros(n_splits)
             kf = KFold(n_splits=n_splits, shuffle=True)
@@ -608,7 +607,7 @@ class TwoStepEstimatorTracking:
             ind_CV = 0
 
             for train_index, test_index in kf.split(grid_split):
-
+                print('-------------- ind_CV:', ind_CV+1)
                 train_index = train_index+1
                 test_index = test_index+1
                 train_index = np.concatenate((np.array([0]), train_index, np.array([len(self.grid[1:-1])+1])))
@@ -617,9 +616,10 @@ class TwoStepEstimatorTracking:
                 Q_test = self.Q[test_index]
                 lbda = np.array([x[1],x[2]])
                 basis_theta, Q_smooth, k = self.__fit(grid_train, Q_train, x[3], x[0], lbda, nb_basis, epsilon=epsilon, max_iter=max_iter)
+                print('end fit')
                 Q_test_pred = solve_FrenetSerret_ODE_SO(basis_theta.evaluate, self.grid, self.Q[0])
                 dist = np.mean(SO3.geodesic_distance(Q_test, Q_test_pred[test_index]))
-
+                print('end distance')
                 score[ind_CV] = dist
                 ind_CV += 1 
 
