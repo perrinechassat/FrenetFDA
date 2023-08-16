f74358c8187c0365fbd15e059da0f2d280be5855
Perrine Chassat <perrine.chassat@gmail.com>
2023-08-16 16:28:57 +0200

    sign language

---
+++ Sign_Language/compute_curvatures.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import numpy as np
+import sys
+sys.path.insert(1, '../')
+from pickle import *
+import time 
+import os.path
+import os
+import dill as pickle
+from tqdm import tqdm
+from utils_functions import estimation_GS_group
+
+
+
+def compute_list_Y_from_group(df, group):
+    list_Y = []
+    for gls in group:
+        n_rep_Thomas = int(df[(df["glose"]==gls) & (df["name"]=='Thomas')]["n_rep"].values[0])
+        for k in range(n_rep_Thomas):
+            list_Y.append(df[(df["glose"]==gls) & (df["name"]=='Thomas')]["Rwrist"].values[0][k])
+        n_rep_Aliza = int(df[(df["glose"]==gls) & (df["name"]=='Aliza')]["n_rep"].values[0])
+        for k in range(n_rep_Aliza):
+            list_Y.append(df[(df["glose"]==gls) & (df["name"]=='Aliza')]["Rwrist"].values[0][k])
+    return list_Y
+
+
+# Load data frame of data
+df = pd.read_pickle('./Process_Data/LSFtraj_5dis_cutrep.pkl')  
+
+group_1 = ["faire l'amour", 'coiffer', 'Dimanche', 'ne pas aimer', 'Toujours', 'Autrefois', 'avril', 'train', "avoir l'air"] #93
+group_2 = ['appeler', 'août', 'avant', 'animal', 'chercher', 'avion', 'besoin', 'asthme', 'bus'] #93
+group_3 = ['bouche bée', 'donner', 'avoir', 'bonjour', 'bouche cousue', 'brosse à dent', 'cerise', 'chaud', 'commander(resto)'] #93
+group_4 = ['aimer', 'autres', 'bleu', 'boire', 'bonbon', 'chagrin', 'chuchoter', 'cloche'] #93
+group_5 = ['eux trois', 'ils', 'commander(qqun)', 'croire', 'ne pas croire', 'curieux', 'jusqu’à', 'il faut', 'elle'] #93
+group_6 = ['après-midi', 'débrouiller', 'éclair', 'enfant', 'désolé', 'décembre', 'jour', 'femme'] #90
+group_7 = ['jamais', 'dire', 'essayer', 'faim', 'je', 'janvier', 'jaune', 'Maman'] #93
+
+list_Y_1 = compute_list_Y_from_group(df, group_1)
+list_Y_2 = compute_list_Y_from_group(df, group_2)
+list_Y_3 = compute_list_Y_from_group(df, group_3)
+list_Y_4 = compute_list_Y_from_group(df, group_4)
+list_Y_5 = compute_list_Y_from_group(df, group_5)
+list_Y_6 = compute_list_Y_from_group(df, group_6)
+list_Y_7 = compute_list_Y_from_group(df, group_7)
+
+
+
+directory = r"results/trial_01/"
+filename_base = "results/trial_01/"
+current_directory = os.getcwd()
+final_directory = os.path.join(current_directory, directory)
+if not os.path.exists(final_directory):
+   os.makedirs(final_directory)
+
+
+n_call_bayopt_der = 30
+bounds_lbda = np.array([[1e-10, 1e-06], [1e-10, 1e-06]])
+n_call_bayopt_theta = 30
+
+#### 1
+
+print('Start: Estimation curvatures with GS on group 1.', '\n')
+
+filename = filename_base + "group_1_"
+estimation_GS_group(filename, list_Y_1, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 1.', '\n')
+
+
+#### 2
+
+print('Start: Estimation curvatures with GS on group 2.', '\n')
+
+filename = filename_base + "group_2_"
+estimation_GS_group(filename, list_Y_2, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 2.', '\n')
+
+
+#### 3
+
+print('Start: Estimation curvatures with GS on group 3.', '\n')
+
+filename = filename_base + "group_3_"
+estimation_GS_group(filename, list_Y_3, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 3.', '\n')
+
+
+#### 4
+
+print('Start: Estimation curvatures with GS on group 4.', '\n')
+
+filename = filename_base + "group_4_"
+estimation_GS_group(filename, list_Y_4, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 4.', '\n')
+
+
+#### 5
+
+print('Start: Estimation curvatures with GS on group 5.', '\n')
+
+filename = filename_base + "group_5_"
+estimation_GS_group(filename, list_Y_5, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 5.', '\n')
+
+
+#### 6
+
+print('Start: Estimation curvatures with GS on group 6.', '\n')
+
+filename = filename_base + "group_6_"
+estimation_GS_group(filename, list_Y_6, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 6.', '\n')
+
+
+#### 7
+
+print('Start: Estimation curvatures with GS on group 7.', '\n')
+
+filename = filename_base + "group_7_"
+estimation_GS_group(filename, list_Y_7, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta)
+
+print('End: Estimation curvatures with GS on group 7.', '\n')
---
+++ Sign_Language/utils_functions.py
@@ -0,0 +1,112 @@
+import sys
+sys.path.insert(1, '../')
+from FrenetFDA.utils.Frenet_Serret_utils import *
+from FrenetFDA.utils.smoothing_utils import *
+from FrenetFDA.processing_Euclidean_curve.unified_estimate_Frenet_state_space.EM_Frenet_state_space_CV import FrenetStateSpaceCV_global
+from FrenetFDA.processing_Euclidean_curve.unified_estimate_Frenet_state_space.iek_filter_smoother_Frenet_state import IEKFilterSmootherFrenetState
+from FrenetFDA.processing_Euclidean_curve.preprocessing import *
+from FrenetFDA.processing_Euclidean_curve.estimate_Frenet_path import GramSchmidtOrthogonalization, ConstrainedLocalPolynomialRegression
+from FrenetFDA.processing_Euclidean_curve.estimate_Frenet_curvatures import ExtrinsicFormulas
+from FrenetFDA.processing_Frenet_path.estimate_Frenet_curvatures import ApproxFrenetODE, LocalApproxFrenetODE
+import FrenetFDA.utils.visualization as visu
+from pickle import *
+import time 
+import os.path
+import os
+import dill as pickle
+from tqdm import tqdm
+import numpy as np
+
+
+def init_arclength_Q(Y, n_call_bayopt):
+
+    grid_time = np.linspace(0,1,Y.shape[0])
+    step = grid_time[1] 
+    bounds_h = [3*step, 0.1]
+
+    ## Init Gamma and s(t)
+    derivatives, h_opt = compute_derivatives(Y, grid_time, deg=3, h=None, CV_optimization_h={"flag":True, "h_grid":np.array([bounds_h[0], bounds_h[-1]]), "K":10, "method":'bayesian', "n_call":n_call_bayopt, "verbose":False})
+    grid_arc_s, L, arc_s, arc_s_dot = compute_arc_length(Y, grid_time, smooth=True, smoothing_param=h_opt)
+    Y_scale = Y/L
+    print('fin arc length')
+
+    ## Z GramSchmidt
+    bounds_h[0] = np.max((bounds_h[0], np.max(grid_arc_s[1:]-grid_arc_s[:-1])))
+    if bounds_h[1] <= bounds_h[0]:
+        bounds_h[1] = np.min((3*bounds_h[0], 0.3))
+    GS_orthog = GramSchmidtOrthogonalization(Y_scale, grid_arc_s, deg=3)
+    h_opt = GS_orthog.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
+    Z_hat_GS, Q_hat_GS, X_hat_GS = GS_orthog.fit(h_opt) 
+    print('fin Z GramSchmidt')
+
+    return grid_arc_s, L, Y_scale, Z_hat_GS, bounds_h
+
+
+
+def basis_GS_leastsquares(grid_arc_s, Z_hat_GS, nb_basis, bounds_h, bounds_lbda, n_call_bayopt):
+    try:
+        local_approx_ode = LocalApproxFrenetODE(grid_arc_s, Z=Z_hat_GS)
+        h_opt, lbda_opt, coefs_opt = local_approx_ode.bayesian_optimization_hyperparameters(n_call_bayopt=n_call_bayopt, lambda_bounds=bounds_lbda, h_bounds=bounds_h, nb_basis=nb_basis, n_splits=10, verbose=False, return_coefs=True)
+        return [coefs_opt, h_opt, lbda_opt]
+    except:
+        return None
+    
+
+
+def estimation_GS(Y, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta):
+
+    grid_arc_s, L, Y_scale, Z_hat_GS, bounds_h = init_arclength_Q(Y, n_call_bayopt_der)
+    nb_basis = int(np.sqrt(Y.shape[0]))
+    res_theta = basis_GS_leastsquares(grid_arc_s, Z_hat_GS, nb_basis, bounds_h, bounds_lbda, n_call_bayopt_theta)
+
+    return grid_arc_s, L, Y_scale, Z_hat_GS, res_theta
+
+
+
+def estimation_GS_group(filename_base, list_Y, n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta):
+
+    N_curves = len(list_Y)
+
+    time_init = time.time()
+
+    with tqdm(total=N_curves) as pbar:
+        res = Parallel(n_jobs=N_curves)(delayed(estimation_GS)(list_Y[k], n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta) for k in range(N_curves))
+    pbar.update()
+
+    time_end = time.time()
+    duration = time_end - time_init
+
+    tab_grid_arc_s = []
+    tab_L = []
+    tab_Y_scale = []
+    tab_Z_hat_GS = []
+    tab_smooth_theta_coefs = []
+    tab_h_opt = []
+    tab_lbda_opt = []
+    for k in range(N_curves):
+        tab_grid_arc_s.append(res[k][0])
+        tab_L.append(res[k][1])
+        tab_Y_scale.append(res[k][2])
+        tab_Z_hat_GS.append(res[k][3])
+        if res[k][4] is not None:
+            tab_smooth_theta_coefs.append(res[k][4][0])
+            tab_h_opt.append(res[k][4][1])
+            tab_lbda_opt.append(res[k][4][2])
+        else:
+            tab_smooth_theta_coefs.append(None)
+            tab_h_opt.append(None)
+            tab_lbda_opt.append(None)
+
+    filename = filename_base + "estimations_GS_leastsquares"
+
+    dic = {"duration":duration, "tab_smooth_theta_coefs":tab_smooth_theta_coefs, "tab_h_opt":tab_h_opt, "tab_lbda_opt":tab_lbda_opt, "tab_grid_arc_s":tab_grid_arc_s, "tab_L":tab_L, 
+            "tab_Y_scale":tab_Y_scale, "tab_Z_hat_GS":tab_Z_hat_GS}
+           
+    if os.path.isfile(filename):
+        print("Le fichier ", filename, " existe déjà.")
+        filename = filename + '_bis'
+    fil = open(filename,"xb")
+    pickle.dump(dic,fil)
+    fil.close()
+
+
