b8bab0f915d5c84e2bceb88235945fb8f26ab440
Perrine Chassat <perrine.chassat@gmail.com>
2023-08-17 18:03:09 +0200

    sign language

--- Sign_Language/utils_functions.py
+++ Sign_Language/utils_functions.py
@@ -28,7 +28,7 @@ def init_arclength_Q(Y, n_call_bayopt):
     derivatives, h_opt = compute_derivatives(Y, grid_time, deg=3, h=None, CV_optimization_h={"flag":True, "h_grid":np.array([bounds_h[0], bounds_h[-1]]), "K":10, "method":'bayesian', "n_call":n_call_bayopt, "verbose":False})
     grid_arc_s, L, arc_s, arc_s_dot = compute_arc_length(Y, grid_time, smooth=True, smoothing_param=h_opt)
     Y_scale = Y/L
-    print('fin arc length')
+    # print('fin arc length')
 
     ## Z GramSchmidt
     bounds_h[0] = np.max((bounds_h[0], np.max(grid_arc_s[1:]-grid_arc_s[:-1])))
@@ -37,7 +37,7 @@ def init_arclength_Q(Y, n_call_bayopt):
     GS_orthog = GramSchmidtOrthogonalization(Y_scale, grid_arc_s, deg=3)
     h_opt = GS_orthog.bayesian_optimization_hyperparameters(n_call_bayopt, h_bounds=bounds_h, verbose=False)
     Z_hat_GS, Q_hat_GS, X_hat_GS = GS_orthog.fit(h_opt) 
-    print('fin Z GramSchmidt')
+    # print('fin Z GramSchmidt')
 
     return grid_arc_s, L, Y_scale, Z_hat_GS, bounds_h
 
@@ -67,35 +67,81 @@ def estimation_GS_group(filename_base, list_Y, n_call_bayopt_der, bounds_lbda, n
 
     N_curves = len(list_Y)
 
+    # time_init = time.time()
+
+    # with tqdm(total=N_curves) as pbar:
+    #     res = Parallel(n_jobs=N_curves)(delayed(estimation_GS)(list_Y[k], n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta) for k in range(N_curves))
+    # pbar.update()
+
+    # time_end = time.time()
+    # duration = time_end - time_init
+
+    # tab_grid_arc_s = []
+    # tab_L = []
+    # tab_Y_scale = []
+    # tab_Z_hat_GS = []
+    # tab_smooth_theta_coefs = []
+    # tab_h_opt = []
+    # tab_lbda_opt = []
+    # for k in range(N_curves):
+    #     tab_grid_arc_s.append(res[k][0])
+    #     tab_L.append(res[k][1])
+    #     tab_Y_scale.append(res[k][2])
+    #     tab_Z_hat_GS.append(res[k][3])
+    #     if res[k][4] is not None:
+    #         tab_smooth_theta_coefs.append(res[k][4][0])
+    #         tab_h_opt.append(res[k][4][1])
+    #         tab_lbda_opt.append(res[k][4][2])
+    #     else:
+    #         tab_smooth_theta_coefs.append(None)
+    #         tab_h_opt.append(None)
+    #         tab_lbda_opt.append(None)
+
     time_init = time.time()
 
     with tqdm(total=N_curves) as pbar:
-        res = Parallel(n_jobs=N_curves)(delayed(estimation_GS)(list_Y[k], n_call_bayopt_der, bounds_lbda, n_call_bayopt_theta) for k in range(N_curves))
+        res = Parallel(n_jobs=N_curves)(delayed(init_arclength_Q)(list_Y[k], n_call_bayopt_der) for k in range(N_curves))
     pbar.update()
 
     time_end = time.time()
     duration = time_end - time_init
+    print('_____________ END Z_Gram_Schmidt ______________')
 
     tab_grid_arc_s = []
     tab_L = []
     tab_Y_scale = []
     tab_Z_hat_GS = []
-    tab_smooth_theta_coefs = []
-    tab_h_opt = []
-    tab_lbda_opt = []
+    tab_bounds_h = []
     for k in range(N_curves):
         tab_grid_arc_s.append(res[k][0])
         tab_L.append(res[k][1])
         tab_Y_scale.append(res[k][2])
         tab_Z_hat_GS.append(res[k][3])
-        if res[k][4] is not None:
-            tab_smooth_theta_coefs.append(res[k][4][0])
-            tab_h_opt.append(res[k][4][1])
-            tab_lbda_opt.append(res[k][4][2])
+        tab_bounds_h.append(res[k][4])
+
+    time_init = time.time()
+
+    with tqdm(total=N_curves) as pbar:
+        res = Parallel(n_jobs=N_curves)(delayed(basis_GS_leastsquares)(tab_grid_arc_s[k], tab_Z_hat_GS[k], int(np.sqrt(list_Y[k].shape[0])), tab_bounds_h[k], bounds_lbda, n_call_bayopt_theta) for k in range(N_curves))
+    pbar.update()
+
+    time_end = time.time()
+    duration = time_end - time_init
+    print('_____________ END Basis Theta ______________')
+
+    tab_smooth_theta_coefs = []
+    tab_h_opt = []
+    tab_lbda_opt = []
+    for k in range(N_curves):
+        if res[k] is not None:
+            tab_smooth_theta_coefs.append(res[k][0])
+            tab_h_opt.append(res[k][1])
+            tab_lbda_opt.append(res[k][2])
         else:
             tab_smooth_theta_coefs.append(None)
             tab_h_opt.append(None)
             tab_lbda_opt.append(None)
+
 
     filename = filename_base + "estimations_GS_leastsquares"
 
