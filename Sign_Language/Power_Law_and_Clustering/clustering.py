4de33259eca7cb90a15fad2719309046a33541e3
Perrine Chassat <perrine.chassat@gmail.com>
2023-11-14 09:06:45 +0100

    simu

--- Sign_Language/Power_Law_and_Clustering/clustering.py
+++ Sign_Language/Power_Law_and_Clustering/clustering.py
@@ -41,24 +41,24 @@ def compute_list_Y_from_group(df, group):
 df = pd.read_pickle('../Process_Data/LSFtraj_5dis_cutrep.pkl')  
 
 group_1 = ["faire l'amour", 'coiffer', 'Dimanche', 'ne pas aimer', 'Toujours', 'Autrefois', 'avril', 'train', "avoir l'air"] #93
-# group_2 = ['appeler', 'août', 'avant', 'animal', 'chercher', 'avion', 'besoin', 'asthme', 'bus'] #93
-# group_3 = ['bouche bée', 'donner', 'avoir', 'bonjour', 'bouche cousue', 'brosse à dent', 'cerise', 'chaud', 'commander(resto)'] #93
-# group_4 = ['aimer', 'autres', 'bleu', 'boire', 'bonbon', 'chagrin', 'chuchoter', 'cloche'] #93
-# group_5 = ['eux trois', 'ils', 'commander(qqun)', 'croire', 'ne pas croire', 'curieux', 'jusqu’à', 'il faut'] #, 'elle'] #93
-# group_6 = ['après-midi', 'débrouiller', 'éclair', 'enfant', 'désolé', 'décembre', 'jour', 'femme'] #90
-# group_7 = ['jamais', 'dire', 'essayer', 'faim', 'je', 'janvier', 'jaune', 'Maman'] #93
+group_2 = ['appeler', 'août', 'avant', 'animal', 'chercher', 'avion', 'besoin', 'asthme', 'bus'] #93
+group_3 = ['bouche bée', 'donner', 'avoir', 'bonjour', 'bouche cousue', 'brosse à dent', 'cerise', 'chaud', 'commander(resto)'] #93
+group_4 = ['aimer', 'autres', 'bleu', 'boire', 'bonbon', 'chagrin', 'chuchoter', 'cloche'] #93
+group_5 = ['eux trois', 'ils', 'commander(qqun)', 'croire', 'ne pas croire', 'curieux', 'jusqu’à', 'il faut'] #, 'elle'] #93
+group_6 = ['après-midi', 'débrouiller', 'éclair', 'enfant', 'désolé', 'décembre', 'jour', 'femme'] #90
+group_7 = ['jamais', 'dire', 'essayer', 'faim', 'je', 'janvier', 'jaune', 'Maman'] #93
 
 list_Y_1, ind_group_1 = compute_list_Y_from_group(df, group_1)
-# list_Y_2, ind_group_2 = compute_list_Y_from_group(df, group_2)
-# list_Y_3, ind_group_3 = compute_list_Y_from_group(df, group_3)
-# list_Y_4, ind_group_4 = compute_list_Y_from_group(df, group_4)
-# list_Y_5, ind_group_5 = compute_list_Y_from_group(df, group_5)
-# list_Y_6, ind_group_6 = compute_list_Y_from_group(df, group_6)
-# list_Y_7, ind_group_7 = compute_list_Y_from_group(df, group_7)
+list_Y_2, ind_group_2 = compute_list_Y_from_group(df, group_2)
+list_Y_3, ind_group_3 = compute_list_Y_from_group(df, group_3)
+list_Y_4, ind_group_4 = compute_list_Y_from_group(df, group_4)
+list_Y_5, ind_group_5 = compute_list_Y_from_group(df, group_5)
+list_Y_6, ind_group_6 = compute_list_Y_from_group(df, group_6)
+list_Y_7, ind_group_7 = compute_list_Y_from_group(df, group_7)
 
 
 max_n_samples = []
-tab_list_Y = [list_Y_1] #, list_Y_2, list_Y_3, list_Y_4, list_Y_5, list_Y_6, list_Y_7]
+tab_list_Y = [list_Y_1, list_Y_2, list_Y_3, list_Y_4, list_Y_5, list_Y_6, list_Y_7]
 for k in range(len(tab_list_Y)):
     max_n_samples.append(np.max([len(tab_list_Y[k][i]) for i in range(len(tab_list_Y[k]))]))
 N = np.max(max_n_samples)
@@ -74,45 +74,47 @@ full_x_reshape = np.load("results/full_x_reshape.npy", allow_pickle=True)
 
 # matrices de pairwise distances
 n_tot_curves = len(full_x_reshape)
-print(n_tot_curves)
+print("n_tot_curves", n_tot_curves)
 
-mat_SRC_scaled = np.zeros((n_tot_curves, n_tot_curves))
-mat_SRC_scaled_bis = np.zeros((n_tot_curves, n_tot_curves))
+mat_SRC_scaled_1 = np.zeros((n_tot_curves, n_tot_curves))
+mat_SRC_scaled_500 = np.zeros((n_tot_curves, n_tot_curves))
+mat_SRVF = np.zeros((n_tot_curves, n_tot_curves))
+mat_FC_scaled = np.zeros((n_tot_curves, n_tot_curves))
+
 # mat_SRC = np.zeros((n_tot_curves, n_tot_curves))
-# mat_SRVF = np.zeros((n_tot_curves, n_tot_curves))
-# mat_FC_scaled = np.zeros((n_tot_curves, n_tot_curves))
 # mat_FC = np.zeros((n_tot_curves, n_tot_curves))
 for i in range(n_tot_curves):
     print(i)
     for j in range(i+1, n_tot_curves):
 
-        # mat_SRVF[i,j] = SRVF(3).dist(full_x_reshape[i], full_x_reshape[j])
-        # concact_arc_s = np.unique(np.round(np.sort(np.concatenate([full_grid_arc_s[i], full_grid_arc_s[j]])), decimals=8))
+        mat_SRVF[i,j] = SRVF(3).dist(full_x_reshape[i], full_x_reshape[j])
+        concact_arc_s = np.unique(np.round(np.sort(np.concatenate([full_grid_arc_s[i], full_grid_arc_s[j]])), decimals=8))
         
         fi = lambda s: interpolate.interp1d(full_grid_arc_s[i], full_theta_scale[i].T)(s).T
         fj = lambda s: interpolate.interp1d(full_grid_arc_s[j], full_theta_scale[j].T)(s).T
-        mat_SRC_scaled[i,j] = SRC(3).dist_bis(fi, fj, full_grid_arc_s_reshape[i], full_grid_arc_s_reshape[j], grid_time, lam=1)
-        mat_SRC_scaled_bis[i,j] = SRC(3).dist_bis(fi, fj, full_grid_arc_s_reshape[i], full_grid_arc_s_reshape[j], grid_time, lam=500)
+        mat_SRC_scaled_1[i,j] = SRC(3).dist_bis(fi, fj, full_grid_arc_s_reshape[i], full_grid_arc_s_reshape[j], grid_time, lam=1)
+        mat_SRC_scaled_500[i,j] = SRC(3).dist_bis(fi, fj, full_grid_arc_s_reshape[i], full_grid_arc_s_reshape[j], grid_time, lam=500)
 
-        # mat_FC_scaled[i,j] = Frenet_Curvatures(3).dist(fi, fj, concact_arc_s)
+        mat_FC_scaled[i,j] = Frenet_Curvatures(3).dist(fi, fj, concact_arc_s)
         
         # fi = lambda s: interpolate.interp1d(full_grid_arc_s[i], full_theta[i].T)(s).T
         # fj = lambda s: interpolate.interp1d(full_grid_arc_s[j], full_theta[j].T)(s).T
         # mat_SRC[i,j] = SRC(3).dist_bis(fi, fj, full_grid_arc_s_reshape[i], full_grid_arc_s_reshape[j], grid_time, lam=100)
         # mat_FC[i,j] = Frenet_Curvatures(3).dist(fi, fj, concact_arc_s)
 
-mat_SRC_scaled = mat_SRC_scaled + mat_SRC_scaled.T
-mat_SRC_scaled_bis = mat_SRC_scaled_bis + mat_SRC_scaled_bis.T
+mat_SRC_scaled_1 = mat_SRC_scaled_1 + mat_SRC_scaled_1.T
+mat_SRC_scaled_500 = mat_SRC_scaled_500 + mat_SRC_scaled_500.T
+mat_SRVF = mat_SRVF + mat_SRVF.T 
+mat_FC_scaled = mat_FC_scaled + mat_FC_scaled.T 
+
 # mat_SRC = mat_SRC + mat_SRC.T
-# mat_SRVF = mat_SRVF + mat_SRVF.T 
-# mat_FC_scaled = mat_FC_scaled + mat_FC_scaled.T 
 # mat_FC = mat_FC + mat_FC.T
 
 
-np.save("results/mat_SRC_scaled_1", mat_SRC_scaled, allow_pickle=True)
-np.save("results/mat_SRC_scaled_500", mat_SRC_scaled_bis, allow_pickle=True)
+np.save("results/mat_SRC_scaled_1_full", mat_SRC_scaled_1, allow_pickle=True)
+np.save("results/mat_SRC_scaled_500_full", mat_SRC_scaled_500, allow_pickle=True)
+np.save("results/mat_SRVF_full", mat_SRVF, allow_pickle=True)
+np.save("results/mat_FC_scaled_full", mat_FC_scaled, allow_pickle=True)
 
 # np.save("results/mat_SRC", mat_SRC, allow_pickle=True)
-# np.save("results/mat_SRVF", mat_SRVF, allow_pickle=True)
-# np.save("results/mat_FC_scaled", mat_FC_scaled, allow_pickle=True)
 # np.save("results/mat_FC", mat_FC, allow_pickle=True)
